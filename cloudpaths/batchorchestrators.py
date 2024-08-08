1d2d216d8a5fd2fb9edd4fd94d4fd3a6ba009b63
David W.H. Swenson <dwhs@hyperblazer.net>
2024-08-08 01:46:24 -0500

    Add batchorchestrators and slurm

---
+++ cloudpaths/batchorchestrators.py
@@ -0,0 +1,64 @@
+import string
+import pathlib
+try:
+    import boto3
+except ImportError:
+    HAS_AWS = False
+else:
+    HAS_AWS = True
+
+
+# need to make these storable, but that should be easy
+class BatchOrchestrator:
+    def __init__(self, template, jobs_dir='.', working_dir='.'):
+        with open(template, mode='r') as f:
+            self.template = string.Template(f.read())
+
+        self.jobs_dir = pathlib.Path(jobs_dir)
+        self.working_dir = pathlib.Path(working_dir)
+
+    def submit_job(self, taskid, dependencies) -> str:
+        """Return the jobid"""
+        raise NotImplementedError()
+
+    def submit_graph(self, taskgraph):
+        task_to_jobid = {}
+        for taskid in taskgraph.execution_order():
+            dependencies = [
+                task_to_jobid[dep] for dep, target in taskgraph.edges
+                if target == taskid
+            ]
+            task_to_jobid[taskid] = self.submit_job(taskid, dependencies)
+
+    def make_executable(self, taskid):
+        raise NotImplementedError()
+
+    def fill_template(self, taskid):
+        jobfile = str(self.jobs_dir / f"{taskid}.job")
+        executable = self.make_executable(taskid)
+        with open(jobfile, mode='w') as f:
+            f.write(self.template.substitute(EXECUTABLE=executable))
+        return jobfile
+
+class AWSBatchOrchestrator(BatchOrchestrator):
+    def __init__(self, template, queue):
+        if not HAS_AWS:
+            raise RuntimeError("AWS integration requires boto3 installed")
+
+        self.queue = queue
+
+        super().__init__(template)
+
+    def submit_job(self, taskid, dependencies):
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/batch/client/submit_job.html
+        resp = boto3.submit_job(
+            jobName=taskid,
+            jobQueue=self.queue,
+            dependsOn=[
+                {'jobId': dep, 'type': 'SEQUENTIAL'}
+                for dep in dependencies
+            ],
+            jobDefinition=..., # TODO: what is the job definition here?
+        )
+        return resp['jobId']
+
---
+++ cloudpaths/slurm.py
@@ -0,0 +1,63 @@
+import subprocess
+
+import click
+from cloudpaths.batchorchestrators import BatchOrchestrator
+from cloudpaths.create_task_graph import create_task_graph
+from cloudpaths.move_to_ops.storage_handlers import LocalFileStorageHandler
+
+from paths_cli.parameters import (
+    INPUT_FILE, SCHEME, INIT_CONDS, OUTPUT_FILE, N_STEPS_MC,
+)
+
+class SLURMOrchestrator(BatchOrchestrator):
+    def make_executable(self, taskid):
+        return (
+            f"python -m cloudpaths.slurm run_task {taskid} --working-dir "
+            f"{self.working_dir}"
+        )
+
+    def submit_job(self, taskid, dependencies):
+        # taskid is a task identified; dependencies are a list of jobids
+        jobfile = self.fill_template(taskid)
+        dependency_str = ":".join(str(dep) for dep in dependencies)
+        cmd = ["sbatch", f"--dependency=afterok:{dependency_str}", jobfile]
+        jobid = subprocess.check_output(cmd)
+        return jobid
+
+
+
+
+@click.group()
+def slurm():
+    pass
+
+@slurm.command()
+@INPUT_FILE.clicked()
+@OUTPUT_FILE.clicked()
+@SCHEME.clicked()
+@INIT_CONDS.clicked()
+@N_STEPS_MC
+@click.option("--template", required=True)
+def submit(input_file, output_file, scheme, init_conds, nsteps, template):
+    base_dir = pathlib.Path(output_file).parent
+    storage_handler = LocalFileStorageHandler(base_dir / "working")
+    objectdb = SimStoreZipStorage(storage_handler)
+    task_graph = create_task_graph(scheme, nsteps, objectdb)
+    OrchestratorClass = CLUSTER_TYPES[cluster_type]
+    orchestrator = SLURMOrchestrator(template, jobs_dir=base_dir / "jobs")
+    orchestrator.submit_graph(task_graph)
+
+
+@slurm.command()
+@click.argument('taskid', type=str)
+@click.option('--working-dir')  # TODO: typing/validation on inputs
+def run_task(taskid, working_dir):
+    storage_handler = LocalFileStorageHandler(working_dir)
+    objectdb = SimStoreZipStorage(storage_handler)
+    # TODO: move the code that does this somewhere else, probably
+    task = MoverTask(taskid)
+    task.run_task(objectdb)
+
+
+if __name__ == "__main__":
+    slurm()
